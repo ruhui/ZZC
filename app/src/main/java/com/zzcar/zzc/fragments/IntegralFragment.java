42eeb8bfd74210740740ce382a4ffd1f0beb616e
ruhui <921263933@qq.com>
2017-05-15 00:09:52 +0800

    车行认证

---
+++ app/src/main/java/com/zzcar/zzc/fragments/IntegralFragment.java
@@ -0,0 +1,19 @@
+package com.zzcar.zzc.fragments;
+
+import com.zzcar.zzc.R;
+import com.zzcar.zzc.fragments.base.BaseFragment;
+
+import org.androidannotations.annotations.EFragment;
+
+/**
+ * 描述：积分
+ * 作者：黄如辉  时间 2017/5/15.
+ */
+
+@EFragment(R.layout.fragment_integral)
+public class IntegralFragment extends BaseFragment{
+    @Override
+    public void onNetChange(int netMobile) {
+
+    }
+}
